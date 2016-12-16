96f830f6148306ef1120cbace4f22488a43aa9c6
adriantobias94 <adriantobias94@gmail.com>
2016-12-17 00:46:22 +0800

    12/17/16

---
+++ src/apc/entjava/photogallery/businesslogic/ContactInterface.java
@@ -0,0 +1,12 @@
+package apc.entjava.photogallery.businesslogic;
+
+import apc.entjava.photogallery.model.Contact;
+
+/**
+ * Created by Adrian on 10/12/2016.
+ */
+public interface ContactInterface {
+
+    void addContact(Contact contact);
+
+}
